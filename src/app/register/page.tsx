686231d04eaa997e896ed4b9d04a27bb02a27d3f
MaaZiJyun <97087257+MaaZiJyun@users.noreply.github.com>
2024-11-17 12:57:20 +0800

    attributes checking finished

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,14 +5,25 @@ import "./register.css";
 import Link from "next/link";
 
 const page = () => {
+  const [role, setRole] = useState("S");
   const [userID, setUserID] = useState("");
   const [password, setPassword] = useState("");
-
   const [errorMessageOnUserID, setErrorMessageOnUserID] = useState("");
+  const [errorMessageOnPwd, setErrorMessageOnPwd] = useState("");
+
+  const buttonCheck = () => {
+    return (
+      userID === "" ||
+      password === "" ||
+      errorMessageOnUserID !== "" ||
+      errorMessageOnPwd !== ""
+    );
+  };
+
   const signUp = async () => {
     try {
       // Call API function getAllWallets
-      const wallets = await API().createAWallet(userID, password);
+      const wallets = await API().createAWallet(role + userID, password);
       console.log("Wallets:", wallets);
     } catch (error) {
       console.error("Error fetching wallets:", error);
@@ -23,16 +34,38 @@ const page = () => {
     const enteredUserID = e.target.value;
     setUserID(enteredUserID);
 
-    if (enteredUserID) {
-      const duplicate = await API().isUserIDDuplicate(enteredUserID);
-      if (duplicate) {
-        setErrorMessageOnUserID(`UserID ${enteredUserID} is already taken.`);
-      } else {
-        setErrorMessageOnUserID(""); // Clear error message if no duplicate
-      }
+    // Regular expression to test if the input contains only numbers
+    if (!/^\d*$/.test(enteredUserID)) {
+      setErrorMessageOnUserID("Please enter a valid number"); // Show error if invalid
     } else {
-      setErrorMessageOnUserID(""); // Clear error message if input is empty
+      if (enteredUserID.length <= 3) {
+        setErrorMessageOnUserID("Please enter 4 digits numbers");
+      } else {
+        const duplicate = await API().isUserIDDuplicate(role + enteredUserID);
+        if (duplicate) {
+          setErrorMessageOnUserID(`UserID ${enteredUserID} is already taken.`);
+        } else {
+          setErrorMessageOnUserID(""); // Clear error message if no duplicate
+        }
+      }
     }
+  };
+
+  const handlePasswordChange = (e: any) => {
+    const enteredPassword = e.target.value;
+    setPassword(enteredPassword);
+    const words = enteredPassword.trim().split(/\s+/);
+    if (words.length <= 4) {
+      setErrorMessageOnPwd("Password must contain more than 4 words");
+      console.log(`${words} is not available`);
+    } else {
+      setErrorMessageOnPwd("");
+      console.log(`${words} is available`);
+    }
+  };
+
+  const handleRoleChange = (newRole: any) => {
+    setRole(newRole);
   };
 
   return (
@@ -42,24 +75,55 @@ const page = () => {
 
         <h2 className="text-lg text-black mb-4">to create an account</h2>
 
+        <div className="mb-4 flex items-center">
+          <label className="block text-gray-500 text-sm mr-2">Role:</label>
+          <label className="mr-4">
+            <input
+              type="checkbox"
+              checked={role === "S"}
+              onChange={() => handleRoleChange("S")}
+              className="mr-1"
+            />
+            Student
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={role === "L"}
+              onChange={() => handleRoleChange("L")}
+              className="mr-1"
+            />
+            Lecturer
+          </label>
+        </div>
+
         <div className="mb-4">
           <label htmlFor="userID" className="block text-gray-500 text-sm mb-1">
             User ID
           </label>
-          <input
-            type="text"
-            id="userID"
-            value={userID}
-            onChange={handleUserIDChange}
-            // onChange={(e) => setUserID(e.target.value)} // Update state on change
-            placeholder="Enter your NetID"
-            className={`w-full p-2 border text-black border-gray-300 rounded focus:outline-none focus:ring ${
-              errorMessageOnUserID
-                ? "focus:ring-red-400"
-                : "focus:ring-blue-400"
-            }`}
-            required
-          />
+          <div className="flex space-x-2">
+            <input
+              type="text"
+              id="readOnlyField"
+              value={role}
+              readOnly
+              className="w-7 p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-400"
+            />
+            <input
+              type="text"
+              id="userID"
+              value={userID}
+              onChange={handleUserIDChange}
+              // onChange={(e) => setUserID(e.target.value)} // Update state on change
+              placeholder="Enter your NetID"
+              className={`w-full p-2 border text-black border-gray-300 rounded focus:outline-none focus:ring ${
+                errorMessageOnUserID
+                  ? "focus:ring-red-400"
+                  : "focus:ring-blue-400"
+              }`}
+              required
+            />
+          </div>
           {errorMessageOnUserID && (
             <p className="mt-1 text-red-500 text-sm">{errorMessageOnUserID}</p>
           )}
@@ -76,11 +140,17 @@ const page = () => {
             type="password"
             id="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)} // Update state on change
+            // onChange={(e) => setPassword(e.target.value)} // Update state on change
+            onChange={handlePasswordChange}
             placeholder="Enter your NetPassword"
-            className="w-full p-2 border text-black border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-400"
+            className={`w-full p-2 border text-black border-gray-300 rounded focus:outline-none focus:ring ${
+              errorMessageOnPwd ? "focus:ring-red-400" : "focus:ring-blue-400"
+            }`}
             required
           />
+          {errorMessageOnPwd && (
+            <p className="mt-1 text-red-500 text-sm">{errorMessageOnPwd}</p>
+          )}
         </div>
 
         <div className="flex items-center mb-4">
@@ -94,7 +164,10 @@ const page = () => {
         <button
           type="button"
           onClick={signUp}
-          className="w-full mt-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-200"
+          disabled={buttonCheck()}
+          className={`w-full mt-4 py-2 ${
+            buttonCheck() ? "bg-gray-200" : "bg-blue-500 hover:bg-blue-600"
+          } text-white font-bold rounded transition duration-200`}
         >
           Sign up
         </button>
