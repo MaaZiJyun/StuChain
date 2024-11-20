da165110fb7a2e90ae6513670226a612be6f0beb
MaaZiJyun <97087257+MaaZiJyun@users.noreply.github.com>
2024-11-20 22:51:18 +0800

    small change

--- src/app/mining/page.tsx
+++ src/app/mining/page.tsx
@@ -6,6 +6,7 @@ import LocalStorage from "../_controllers/LocalStorage";
 import Spinner from "../_components/Spinner";
 import BlockchainInfo from "../_components/BlockchainInfo";
 import MiningButton from "../_components/MiningButton";
+import WithAuth from "../_components/WithAuth";
 
 const page = () => {
   const [user, setUser] = useState<UserClass>();
@@ -28,28 +29,28 @@ const page = () => {
       <div className="w-full h-screen bg-blue-500">
         <Spinner size="h-20 w-20" color="text-white" strokeWidth={2} />
       </div>
+    ); // Display loading indicator
+  } else
+    return (
+      <div className="h-screen lg:flex bg-blue-600">
+        <Navbar />
+        <main className="overflow-y-auto w-full lg:flex-grow p-6 bg-gray-100 lg:rounded-l-xl lg:my-3 shadow-md">
+          <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+            <h2 className="text-2xl font-bold mb-4">Mining & Coins</h2>
+            <p className="text-gray-600 mb-4">
+              This page allows you to mine new blocks and view blockchain
+              information.
+            </p>
+          </div>
+
+          {/* Add Mining Button component */}
+          <MiningButton />
+
+          {/* Add BlockchainInfo component */}
+          <BlockchainInfo />
+        </main>
+      </div>
     );
-  }
-
-  return (
-    <div className="h-screen lg:flex bg-blue-600">
-      <Navbar />
-      <main className="overflow-y-auto w-full lg:flex-grow p-6 bg-gray-100 lg:rounded-l-xl lg:my-3 shadow-md">
-        <div className="bg-white shadow-md rounded-lg p-6 mb-6">
-          <h2 className="text-2xl font-bold mb-4">Mining & Coins</h2>
-          <p className="text-gray-600 mb-4">
-            This page allows you to mine new blocks and view blockchain information.
-          </p>
-        </div>
-
-        {/* Add Mining Button component */}
-        <MiningButton />
-
-        {/* Add BlockchainInfo component */}
-        <BlockchainInfo />
-      </main>
-    </div>
-  );
 };
 
-export default page;
+export default WithAuth(page);
