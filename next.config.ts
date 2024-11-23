6a91590462e04fd499e73873cbd0622c112d4fd1
MaaZiJyun <97087257+MaaZiJyun@users.noreply.github.com>
2024-11-23 19:40:55 +0800

    server config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+const fs = require('fs');
+const path = require('path');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpackDevMiddleware: (config: any) => {
+    // Enable HTTPS in development
+    config.devMiddleware = {
+      publicPath: '/',
+      https: {
+        key: fs.readFileSync(path.join(__dirname, './ssl/key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, './ssl/cert.pem')),
+      },
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
